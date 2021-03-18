f2f2a99edb7a0dd69c3fe525be056671609b210e
Pavan Yejare <yejare.pavan@gmail.com>
2021-03-19 00:12:52 +0530

    added setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\\[]"`
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\\"`
+NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\"`
 java -jar target/${NAME}-${VERSION}.jar
